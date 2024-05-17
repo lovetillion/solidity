28bd8f8f3bf73c474d55d991265010c881d9c9b0
thejourker <smartyshirts@icloud.com>
2024-05-17 17:21:42 -0400

    Drastically simplified for the v1 for initial supply, mint, burn with ownership

--- contracts/XLV-v1.sol
+++ contracts/XLV-v1.sol
@@ -1,21 +1,23 @@
 // SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
 
-// v1 contract to mirror current CashToken setup on BCH
-contract XLVToken is ERC20, Ownable {
+/// @custom:security-contact support@lovetillion.org
+contract Lovetillion is ERC20, ERC20Burnable, Ownable, ERC20Permit {
     using Math for uint256;
 
-    // Defines the maximum tokens per linear emission schedule
-    uint256 public constant MAX_SUPPLY = 2100000000000000;
-
-    constructor(uint256 initialSupply, address owner) ERC20("Lovetillion", "XLV") Ownable(owner){
-        // Mints the initial supply of tokens directly to the contract deployer
-        // Will be used during deployment to issue an initial set of tokens for use in LP
-        _mint(owner, initialSupply);
+    constructor(address initialOwner)
+        ERC20("Lovetillion", "XLV")
+        Ownable(initialOwner)
+        ERC20Permit("Lovetillion")
+    {
+        _mint(msg.sender, 50 * 10 ** decimals());
     }
 
     // Ensures the same precision as Bitcoin Satoshis is utilized
@@ -23,30 +25,7 @@ contract XLVToken is ERC20, Ownable {
         return 8;
     }
 
-    // Mints XLV tokens (only callable by the contract owner)
-    // This function allows the contract owner to manage the token supply for specific purposes.
-    function mint(uint256 amount) public onlyOwner returns (bool) {
-        // Tracks the total number of XLV tokens currently minted
-        uint256 XLV_SUPPLY = totalSupply();
-
-        if (XLV_SUPPLY + amount <= MAX_SUPPLY)
-        {
-            _mint(msg.sender, amount);
-            return true;
-        }
-        else
-        {
-            revert("Exceeds Maximum Supply");
-        }
-    }
-
-    receive() external payable {
-        // Unexpected/not implemented
-        revert("Function not implemented");
-    }
-
-    fallback() external payable {
-        // Unexpected/not implemented
-        revert("Function not implemented");
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 }
